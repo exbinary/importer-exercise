40e2b9ae8f94624875dc49d9bcea0b6619239b54
exbinary <exbinary@gmail.com>
2014-01-29 15:20:18 -0500

    Rails new -T; Move README.rdoc -> README.md

---
+++ README.md
@@ -0,0 +1,22 @@
+# TPT to LZ Purchase Importer
+
+## todo: ....
+
+## Things you may want to cover:
+
+* Ruby version
+
+* System dependencies
+
+* Configuration
+
+* Database creation
+
+* Database initialization
+
+* How to run the test suite
+
+* Services (job queues, cache servers, search engines, etc.)
+
+* Deployment instructions
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Importer::Application.config.secret_key_base = '593557bdc0d8fb8b1485f80efdebccc1e743ff897feaa90f24a9072bb1f44eb147d40474595a1b9dde63e29cd278da99d0aef2233dce6667e538888049a91d31'
