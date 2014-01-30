bcd0edf2d06924629177473fbee3858a277ba065
exbinary <exbinary@gmail.com>
2014-01-30 09:55:44 -0500

    Add helper to toggle log-in/out link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def link_to_sign_in_or_out
+    if user_signed_in?
+      link_to('Log Out', destroy_user_session_path, :method => :delete)
+    else
+      link_to('Log In', new_user_session_path)
+    end
+  end
+
 end
